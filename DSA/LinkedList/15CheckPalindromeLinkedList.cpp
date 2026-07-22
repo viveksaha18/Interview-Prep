61896bf6990b6e17c9476b346515d9ae8aab5e58
Vivek Saha <viveksaha096@gmail.com>
2026-07-22 04:56:06 +0000

    Check Palindrome Problem And Reverse Linked List Logic Image Pushed

---
+++ DSA/LinkedList/15CheckPalindromeLinkedList.cpp
@@ -0,0 +1,45 @@
+
+
+/**
+* Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* reverse(ListNode* head) {
+        // Using recursive method
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* newHead = reverse(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = nullptr;
+        return newHead;
+}
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second != nullptr) {
+            if(first->val != second->val) {
+                reverse(newHead);
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        reverse(newHead);
+        return true;
+    }
+};
